c461860844b1dd9c04c1d628083ded4f670fc3f6
nathan-gearing <161641604+nathan-gearing@users.noreply.github.com>
2024-11-18 14:20:24 -0500

    v0.02.1.5
    more tutorial work

--- Hospital Hero/Assets/Scripts/Interactables/Collectible.cs
+++ Hospital Hero/Assets/Scripts/Interactables/Collectible.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Collectible : Interactable
 {
-    public float promptTime = 2f;
+    public float promptTime = 15f;
     public float loadTime = 2.5f;
     private UIManager uiManager;
     private GameManager gM;
@@ -21,8 +21,8 @@ public class Collectible : Interactable
     
     protected override void Interact()
     {
-        uiManager.DisplayPrompt("<b>Next Level You Go</b>", promptTime);
-        gM.RequestLevelTransition(1, loadTime);
+        uiManager.DisplayPrompt("Handwashing with soap and water is more effective than using hand sanitizer\r\nSoap is the real hero!", promptTime);
+       
         Destroy(gameObject);
         
         
@@ -31,6 +31,6 @@ public class Collectible : Interactable
     
     protected override string GetInteractPrompt()
     {
-        return "Press I to interact";
+        return "Press I to collect";
     }
 }
